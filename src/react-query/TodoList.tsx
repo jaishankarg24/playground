55dc17bcddf564b4429172aeec670dedc51ce9ac
jaishankar <jaishankarg24@gmail.com>
2023-05-13 11:09:15 +0530

    Starter

---
+++ src/react-query/TodoList.tsx
@@ -0,0 +1,51 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+interface Todo {
+  id: number;
+  title: string;
+  userId: number;
+  completed: boolean;
+}
+
+const TodoList = () => {
+  const fetchTodos = () =>
+    axios
+      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.data);
+
+  const {
+    data: todos,
+    error,
+    isLoading,
+  } = useQuery<Todo[], Error>({
+    queryKey: ["todos"],
+    queryFn: fetchTodos,
+  });
+
+  // const [todos, setTodos] = useState<Todo[]>([]);
+  // const [error, setError] = useState("");
+
+  // useEffect(() => {
+  //   axios
+  //     .get("https://jsonplaceholder.typicode.com/todos")
+  //     .then((res) => setTodos(res.data))
+  //     .catch((error) => setError(error));
+  // }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <ul className="list-group">
+      {todos?.map((todo) => (
+        <li key={todo.id} className="list-group-item">
+          {todo.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
